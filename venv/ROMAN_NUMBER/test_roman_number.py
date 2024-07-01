251b21b3ec2d4aba92cdb18e2af71ebabfe878cc
David Nieto <davidnieto2211@gmail.com>
2024-07-01 15:39:48 -0400

    segund commit

--- venv/ROMAN_NUMBER/test_roman_number.py
+++ venv/ROMAN_NUMBER/test_roman_number.py
@@ -1,4 +1,4 @@
-From foman_funcs import to_roman
+From roman_funcs import to_roman
 	
 Def test_romanos_simples():
 	assert to_roman(1) == ‘I’
@@ -8,3 +8,17 @@ Def test_romanos_simples():
 	assert to_roman(100) == ‘C'
 	assert to_roman(500) == ‘D’
 	assert to_roman(500) == ‘M’
+'''
+from roman_funcs import to_roman
+	
+Def test_romanos_simples():
+	assert to_roman(1) == ‘I’
+	assert to_roman(2) == ‘II’
+	assert to_roman(3) == 'III’
+	assert to_roman(4) == ‘IV'
+	assert to_roman(5) == ‘V'
+	assert to_roman(6) == ‘VI’
+	assert to_roman(7) == ‘VII’
+	assert to_roman(8) == ‘VIII’
+	assert to_roman(9) == ‘IX’
+	'''
